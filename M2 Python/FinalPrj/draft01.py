b264859ca94156b880244e1cdd47a41d0b421e79
vicki <Vicrogatinskaya@gmail.com>
2022-11-02 12:57:21 +0100

    added some logic

--- M2 Python/FinalPrj/draft01.py
+++ M2 Python/FinalPrj/draft01.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 # races = ["Human", "Dwarf", "Halfling", "Elf", "Tiefling", "Orc"]
 # classes = ["Fighter", "Wizard", "Rogue", "Druid", "Barbarian", "Sorcerer", "Bard", "Cleric", "Monk", "Paladin"]
@@ -6,17 +7,74 @@ import random
 
 # ability score calculator-ish????
 
-scores = []
-for i in range(6):
-    score = []
-    for j in range(4): 
-        score.append(random.randint(1,6))
+def __getAbilityScores__(characterClass):
+    scores = []
+    for i in range(6):
+        score = []
+        for j in range(4): 
+            score.append(random.randint(1,6))
 
-    score = sorted(score)
-    score.pop(0) #removing the smallest value score
-    scores.append(sum(score)) # and summing all the values together and saving them above
+        score = sorted(score)
+        score.pop(0) #removing the smallest value score
+        scores.append(sum(score)) # and summing all the values together and saving them above
 
-scores = sorted(scores)
-characterScores = {'STR':0,'DEX':0,'CON':0,'INT':0,'WIS':0,'CHA':0}
+    scores = sorted(scores)
+    characterScores = {'STR':0,'DEX':0,'CON':0,'INT':0,'WIS':0,'CHA':0}
 
-print(scores) #see all random generated values here
+    match characterClass: #hard coded for now to test 
+        case "Fighter":
+            characterScores["STR"] = scores[5]
+            characterScores["CON"] = scores[4]
+            characterScores["DEX"] = scores[3]
+            characterScores["WIS"] = scores[2]
+            characterScores["CHA"] = scores[1]
+            characterScores["INT"] = scores[0]
+
+        case "Wizard":
+            characterScores["INT"] = scores[5]
+            characterScores["CON"] = scores[4]
+            characterScores["DEX"] = scores[3]
+            characterScores["WIS"] = scores[2]
+            characterScores["CHA"] = scores[1]
+            characterScores["STR"] = scores[0]
+        case "Rogue":
+            characterScores["DEX"] = scores[5]
+            characterScores["CON"] = scores[4]
+            characterScores["WIS"] = scores[3]
+            characterScores["CHA"] = scores[2]
+            characterScores["INT"] = scores[1]
+            characterScores["STR"] = scores[0]
+
+    return characterScores
+
+
+# loading the json data
+
+data = open('/Users/victoriarogatinskaya/Desktop/AmazeenCode2022/M2 Python/FinalPrj/data.json')
+load = json.load(data)
+
+for i in load['races']:
+    print(i)
+
+for j in load['classes']:
+    print(j)
+
+
+data.close() # loads ok 
+
+# character class to store data
+
+class playerCharacter:
+    def __init__(self):
+        self.name = " "
+        self.gender = " "
+        self.age = " "
+        self.race = " "
+        self.charClass = " " #class isn't a WORD TO USE
+        self.abilityScores = __getAbilityScores__(self.charClass) #-> test later
+
+newCharacter = playerCharacter()
+newCharacter.name = "Alex" 
+newCharacter.charClass = "Wizard" # this doesnt work
+print(__getAbilityScores__("Wizard")) # this does
+print(vars(newCharacter))
