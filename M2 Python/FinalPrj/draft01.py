3f89089e599af58be8582111f296d437277b673a
Vicka <vicrogatinskaya@gmail.com>
2022-11-03 02:21:51 +0100

    pain

--- M2 Python/FinalPrj/draft01.py
+++ M2 Python/FinalPrj/draft01.py
@@ -7,7 +7,7 @@ import json
 
 # ability score calculator-ish????
 
-def __getAbilityScores__(characterClass):
+def __getAbilityScores__():
     scores = []
     for i in range(6):
         score = []
@@ -20,54 +20,54 @@ def __getAbilityScores__(characterClass):
 
     scores = sorted(scores)
     characterScores = {'STR':0,'DEX':0,'CON':0,'INT':0,'WIS':0,'CHA':0}
+    return(scores)
 
-    match characterClass: #hard coded for now to test ig 
-        case "Fighter":
-            characterScores["STR"] = scores[5]
-            characterScores["CON"] = scores[4]
-            characterScores["DEX"] = scores[3]
-            characterScores["WIS"] = scores[2]
-            characterScores["CHA"] = scores[1]
-            characterScores["INT"] = scores[0]
+    # match characterClass: #hard coded for now to test ig 
+    #     case "Fighter":
+    #         characterScores["STR"] = scores[5]
+    #         characterScores["CON"] = scores[4]
+    #         characterScores["DEX"] = scores[3]
+    #         characterScores["WIS"] = scores[2]
+    #         characterScores["CHA"] = scores[1]
+    #         characterScores["INT"] = scores[0]
 
-        case "Wizard":
-            characterScores["INT"] = scores[5]
-            characterScores["CON"] = scores[4]
-            characterScores["DEX"] = scores[3]
-            characterScores["WIS"] = scores[2]
-            characterScores["CHA"] = scores[1]
-            characterScores["STR"] = scores[0]
-        case "Rogue":
-            characterScores["DEX"] = scores[5]
-            characterScores["CON"] = scores[4]
-            characterScores["WIS"] = scores[3]
-            characterScores["CHA"] = scores[2]
-            characterScores["INT"] = scores[1]
-            characterScores["STR"] = scores[0]
+    #     case "Wizard":
+    #         characterScores["INT"] = scores[5]
+    #         characterScores["CON"] = scores[4]
+    #         characterScores["DEX"] = scores[3]
+    #         characterScores["WIS"] = scores[2]
+    #         characterScores["CHA"] = scores[1]
+    #         characterScores["STR"] = scores[0]
+    #     case "Rogue":
+    #         characterScores["DEX"] = scores[5]
+    #         characterScores["CON"] = scores[4]
+    #         characterScores["WIS"] = scores[3]
+    #         characterScores["CHA"] = scores[2]
+    #         characterScores["INT"] = scores[1]
+    #         characterScores["STR"] = scores[0]
 
-    return characterScores
+    # return characterScores
 
 
 # loading the json data
 
-data = open('/Users/victoriarogatinskaya/Desktop/AmazeenCode2022/M2 Python/FinalPrj/data.json')
-load = json.load(data)
+# data = open('/Users/victoriarogatinskaya/Desktop/AmazeenCode2022/M2 Python/FinalPrj/data.json')
+# load = json.load(data)
 
-for i in load['races']:
-    print(i)
+# for i in load['races']:
+#     print(i)
 
-for j in load['classes']:
-    print(j)
+# for j in load['classes']:
+#     print(j)
 
 
-data.close() # loads ok 
+# data.close() # loads ok 
 
 # character class to store data
 
 class playerCharacter:
     def __init__(self):
         self.name = " "
-        self.gender = " "
         self.age = " "
         self.race = " "
         self.charClass = " " #class isn't a WORD TO USE
@@ -83,12 +83,12 @@ class playerCharacter:
 
 newCharacter = playerCharacter()
 
-newCharacter.name = input("Input the character name: ")
-newCharacter.gender = input("Input your character gender: ")
-newCharacter.age = input("Choose your character age: ")
-newCharacter.race = input("Choose your character's race: ")
-newCharacter.charClass = input("What's your character class: ")
-newCharacter.abilityScores = __getAbilityScores__(newCharacter.charClass)
+# newCharacter.name = input("Input the character name: ")
+# newCharacter.age = input("Choose your character age: ")
+# newCharacter.race = input("Choose your character's race: ")
+# newCharacter.charClass = input("What's your character class: ")
+# newCharacter.abilityScores = __getAbilityScores__(newCharacter.charClass)
 
+print(__getAbilityScores__())
 print(f"Here's your basic character sheet: ")
 print(vars(newCharacter))
